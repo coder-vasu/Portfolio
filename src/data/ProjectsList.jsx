b28f5d9edabb2f92c1220a21ef88c86bb44a2812
Vasu Bhatia <vasubhatia@dldavppcbsei.com>
2024-12-01 01:45:10 +0530

    first commit

---
+++ src/data/ProjectsList.jsx
@@ -0,0 +1,80 @@
+export const ProjectsList = {
+  projects: [
+    {
+      name: "ObjeTrace",
+      description:
+        "A real-time object detection system that identifies multiple objects simultaneously in live video streams, achieving high accuracy and efficient processing through deep learning techniques.",
+      technologies: [
+        "Object Detection",
+        "Deep Learning",
+        "Python",
+        "Machine Learning",
+        "Real-Time Systems",
+        "OpenCV",
+        "TensorFlow",
+        "PyTorch",
+        "Computer Vision",
+      ],
+    },
+    {
+      name: "Saksham - Web Platform for Disability Support",
+      description:
+        "A prototype web platform designed to assist individuals with disabilities by offering resource tracking, geolocation-based support, and emergency alert features, ensuring timely access to essential services.",
+      technologies: [
+        "React JS",
+        "Accessibility Features",
+        "Front-End Technologies",
+        "Back-End Development",
+        "User Experience Design",
+        "Geolocation",
+        "Prosthetics Resource Tracking",
+        "Emergency Alert Integration"
+      ],
+    },{
+      name: "Blog App",
+      description:
+        "A blog application that allows users to create, read, update, and delete posts, integrated with Appwrite for backend services like user authentication, database management, and file storage.",
+      technologies: [
+        "React",
+        "Vite",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Appwrite",
+        "Node.js",
+        "Authentication",
+        "CRUD Operations",
+        "API Integration"
+      ],
+    },
+    {
+      name: "Multi-Socket Client-Server Communication System",
+      description:
+      "A multi-socket server capable of handling multiple clients simultaneously for real-time message exchange, featuring non-blocking communication and robust error-handling mechanisms for reliability under network interruptions.",
+      technologies: [
+        "C++",
+        "Winsock API",
+        "Socket Programming",
+        "Multi-threading",
+        "Network Communication",
+        "Error Handling",
+        "Real-Time Systems",
+        "Non-blocking Communication",
+      ],
+    },
+    {
+      name: "Netflix-Clone",
+      description:
+        "This project is a simplified front-end clone of Netflix. It was created with React js. It uses TMDB (The Movie Data Base ) API and uses the Axios tool. Users can click movie images with embedded YouTube trailers or related videos about the movie.",
+      technologies: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Material-UI",
+        "API Integration",
+        "Git",
+        "GitHub",
+      ],
+    },
+  ],
+};
---
+++ src/data/UserData.jsx
@@ -0,0 +1,40 @@
+export const UserData = {
+  name: "Vasu Bhatia",
+  about : "I'm a skilled web developer with a strong foundation in C++, Python, and modern web technologies like HTML5, CSS3, and JavaScript. I enjoy building efficient and scalable solutions using frameworks like React, Node.js, and Express.js, and I'm also able to work with databases such as MongoDB and MySQL. With a keen interest in machine learning, I also have experience leveraging tools like TensorFlow and PyTorch to solve complex problems. My goal is to create innovative, user-focused applications that make an impact.",
+  resumeUrl : "https://drive.google.com/file/d/18GnLNsmnM0pRtKhfQSmgidT7HTLXh4tA/view",
+  FooterLink : "https://www.linkedin.com/in/VasuBhatia16/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "https://github.com/Vasubhatia16",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/in/vasubhatia16",
+      icon: "FaLinkedinIn",
+    },
+    {
+      socialMediaName: "twitter",
+      url: "https://twitter.com/Vasu_Bhatia_",
+      icon: "AiOutlineTwitter",
+    },
+    {
+      socialMediaName: "instagram",
+      url: "https://instagram.com/vasu_bhatia16",
+      icon: "AiFillInstagram",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Web Developer",
+      "Competitve Coder",
+      "Full Stack Developer",
+      "Machine Learning Enthusiast",
+      "Chess Player",
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
